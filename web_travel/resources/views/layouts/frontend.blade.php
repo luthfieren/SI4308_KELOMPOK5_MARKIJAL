17068b5c47a565924cebcae19893ed70eb616e86
luthfieren <luthfieren@gmail.com>
2022-01-09 15:59:57 +0700

    frontend

--- web_travel/resources/views/layouts/frontend.blade.php
+++ web_travel/resources/views/layouts/frontend.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,72 +8,76 @@
     <title>MARKIJAL Web</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
+
 <body>
     {{-- navbar --}}
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-          <a class="navbar-brand" href="#">MARKIJAL<span class="text-primary">Web</span></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ route('paket.index') }}">Paket</a>
-              </li>
-              @guest
-              <li class="nav-item">
-                <a href="{{ route('register') }}" class=" btn btn-outline-primary">Register</a>
-            </li>
-              <li class="nav-item">
-                  <a href="{{ route('login') }}" class="btn btn-primary ms-2">Login</a>
-              </li>
-              @endguest
-              @auth
-              <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ Auth::user()->name }}
-                </a>
+            <a class="navbar-brand" href="#"><img src="https://i.postimg.cc/RCPDsjpd/markijal-logo.png" alt=""
+                    style="max-width: 100px;"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('paket.index') }}">Paket</a>
+                    </li>
+                    @guest
+                    <li class="nav-item">
+                        <a href="{{ route('register') }}" class=" btn btn-outline-primary">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('login') }}" class="btn btn-primary ms-2">Login</a>
+                    </li>
+                    @endguest
+                    @auth
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
 
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a href="{{ route('profile.index') }}" class="dropdown-item">Profile</a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a href="{{ route('profile.index') }}" class="dropdown-item">Profile</a>
 
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                       onclick="event.preventDefault();
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                      document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
+                                {{ __('Logout') }}
+                            </a>
 
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                </div>
-            </li>
-              @endauth
-            </ul>
-          </div>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                    @endauth
+                </ul>
+            </div>
         </div>
-      </nav>
+    </nav>
 
-      @yield('content')
+    @yield('content')
 
-      {{-- footer --}}
-      <footer class="text-center text-white " style="background-color: #21081a; margin-top :2cm">
+    {{-- footer --}}
+    <footer class="text-center text-white " style="background-color: #21081a; margin-top :2cm">
         <!-- Grid container -->
         <div class="container p-4"></div>
         <!-- Grid container -->
         <!-- Copyright -->
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-          © 2021 Copyright:
-          <a class="text-white" href="#">MARKIJAL.com</a>
+            © 2021 Copyright:
+            <a class="text-white" href="#">MARKIJAL.com</a>
         </div>
         <!-- Copyright -->
-      </footer>
-      
+    </footer>
+
 
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
--- web_travel/resources/views/welcome.blade.php
+++ web_travel/resources/views/welcome.blade.php
@@ -1,77 +1,108 @@
 @extends('layouts.frontend')
 
 @section('content')
-<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" >
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://travelumroh.co.id/wp-content/uploads/2021/03/10.-Ketahui-Perbedaan-Haji-dan-Umroh-Sebelum-Menunaikannya.jpg" height="600" class=" w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" style="background: black;">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+            aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+            aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+            aria-label="Slide 3"></button>
     </div>
-    <div class="carousel-item">
-      <img src="https://i.pinimg.com/originals/e1/73/d2/e173d2e591c8d666e6c0b8ef12239e62.jpg" height="600" class=" w-100" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="https://travelumroh.co.id/wp-content/uploads/2021/03/10.-Ketahui-Perbedaan-Haji-dan-Umroh-Sebelum-Menunaikannya.jpg"
+                class="d-block w-100" alt="..." style="opacity: 0.5; height: 1080px;">
+            <div class="carousel-caption d-none d-md-block" style="opacity: 1;">
+                <h5>HAJI STANDARD</h5>
+                <p>HAJI merupakan jenis ibadah yang dilakukan oleh umat muslim. Ibadah haji didahului oleh puasa asyura
+                    yang jatuh pada tanggal 10 dzulhijjah. Lama waktu ibadah haji adalah sekitar 40 hari sehingga banyak
+                    calon jamaah yang memilih agen travel untuk mengurus perjalanan mereka. Berikut beberapa contoh
+                    kalimat iklan untuk promosi agent travel haji.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="https://wallpaperaccess.com/full/1408398.jpg" class="d-block w-100" alt="..."
+                style="opacity: 0.5;height: 1080px;">
+            <div class="carousel-caption d-none d-md-block" style="opacity: 1;">
+                <h5>BALON UDARA CAPPADOCIA</h5>
+                <p>Berlokasi di barat daya Kota Kayseri, Cappadocia merupakan tujuan wisata populer di Turki. Kota ini
+                    sangat kental dengan nilai sejarah yang unik. Cappadocia berarti tanah kuda yang indah. Sebab, kota
+                    ini punya banyak kuda liar yang berkeliaran di daerah tersebut sejak berabad-abad lalu. Selain kuda
+                    liar, salah satu ciri khas yang paling dikenali dari Cappadocia adalah balon udara warna-warni yang
+                    memeriahkan langit kota ini.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="https://wallpapercave.com/wp/wp9829655.jpg" class="d-block w-100" alt="..."
+                style="opacity: 0.5;height: 1080px;">
+            <div class="carousel-caption d-none d-md-block" style="opacity: 1;">
+                <h5>7 SUMMIT, Rinjani</h5>
+                <p>Indonesia memang kaya akan keindahan alamnya. Gunung, laut, persawahan, semuanya memperlihatkan
+                    betapa indahnya alam Indonesia. Salah satu pesona alam Indonesia adalah Gunung Rinjani. Gunung
+                    dengan ketinggian 3.726 mdpl ini, memang merupakan salah satu pilihan bagi para pendaki, baik
+                    pendaki dalam negeri maupun dari luar negeri. Bagaimana tidak, cerita yang tersebar tentang
+                    keindahannya, membuat setiap pendaki pasti tertantang untuk mengeksplorasi gunung yang terletak di
+                    Lombok, Nusa Tenggara Barat ini.</p>
+            </div>
+        </div>
     </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
 </div>
+
 <div class="container mt-5">
-  <div class="card">
-      <div class="card-header d-flex justify-content-between align-items-center">
-          <span>
-              Paket Tersedia
-          </span>
-          <span>
-              <a href="{{ route('paket.index') }}" class="btn btn-primary">View More</a>
-          </span>
-      </div>
-      <div class="card-body row align-items-center">
-          @foreach ($packages as $item)
-          <div class="col-sm-4">
-          <div class="card">
-            <img src="{{ Storage::url($item->category->foto) }}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">{{ $item->nama_paket }}</h5>
-              <p class="card-text">{{ $item->tujuan }}</p>
-              <p class="card-text">
-                Stok Tersedia : <span class="text-danger">{{ $item->stok }}</span>
-              </p>
-              <p>
-                Destinasi : {{$item->tujuan}}
-              </p>
+    <div class="card">
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <span>
+                Paket Tersedia
+            </span>
+            <span>
+                <a href="{{ route('paket.index') }}" class="btn btn-primary">View More</a>
+            </span>
+        </div>
+        <div class="card-body row align-items-center">
+            @foreach ($packages as $item)
+            <div class="col-sm-4">
+                <div class="card">
+                    <img src="{{ Storage::url($item->category->foto) }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $item->nama_paket }}</h5>
+                        <p class="card-text">{{ $item->tujuan }}</p>
+                        <p class="card-text">
+                            Stok Tersedia : <span class="text-danger">{{ $item->stok }}</span>
+                        </p>
+                        <p>
+                            Destinasi : {{$item->tujuan}}
+                        </p>
+                    </div>
+                    <div class="card-footer d-flex align-items-center justify-content-between">
+                        @auth
+                        @if ($item->stok == 0)
+                        <button class="btn btn-primary" disabled>Stok Habis</button>
+                        @else
+                        <a href="{{ route('paket.show', $item->id) }}" class="btn btn-outline-primary">Detail</a>
+                        <form action="{{ route('paket.create') }}" method="get">
+                            <input type="hidden" name="paket_id" value="{{ $item->id }}">
+                            <button class="btn btn-primary">Beli Sekarang</button>
+                        </form>
+                        @endif
+                        @endauth
+                        @guest
+                        <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
+                        @endguest
+                    </div>
+                </div>
             </div>
-            <div class="card-footer d-flex align-items-center justify-content-between">
-              @auth
-                @if ($item->stok == 0)
-                    <button class="btn btn-primary" disabled>Stok Habis</button>
-                @else
-                <a href="{{ route('paket.show', $item->id) }}" class="btn btn-outline-primary">Detail</a>
-                <form action="{{ route('paket.create') }}" method="get">
-                    <input type="hidden" name="paket_id" value="{{ $item->id }}">
-                    <button class="btn btn-primary">Beli Sekarang</button>
-                </form>
-                @endif
-              @endauth
-              @guest
-                  <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
-              @endguest
-            </div>
-          </div>
-          </div>
-          @endforeach
-      </div>
-  </div>
+            @endforeach
+        </div>
+    </div>
 </div>
 @endsection
